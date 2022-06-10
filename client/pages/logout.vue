d333a25f4078f4a79fe7bc553bf3876e77f7e654
gmarcha <gmarcha@github.com>
2022-06-10 15:57:55 -0400

    Add logout page
    Empty page, simply remove all cookies
    and redirect to login page.

---
+++ client/pages/logout.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'LoginPage',
+
+  asyncData({ store, redirect }) {
+    store.dispatch('logout')
+    redirect('/login')
+  }
+})
+</script>
