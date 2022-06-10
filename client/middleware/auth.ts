bd8fc159fa6f71cd66d621bf65f160c72a008b03
gmarcha <gmarcha@github.com>
2022-06-10 18:12:56 -0400

    Fix middleware routine

--- client/middleware/auth.ts
+++ client/middleware/auth.ts
@@ -3,11 +3,11 @@ import { Middleware } from '@nuxt/types'
 const auth: Middleware = async ({ $cookies, redirect, store }) => {
   const accessToken = $cookies.get('access_token')
   if (!accessToken) redirect('/login')
-  store.commit('auth/AUTH_SUCCESS', { accessToken })
+  store.commit('AUTH_SUCCESS', { accessToken })
   try {
     await store.dispatch('user/fetchAuth')
   } catch (err) {
-    store.dispatch('auth/logout')
+    store.dispatch('logout')
     redirect('/login')
   }
 }
