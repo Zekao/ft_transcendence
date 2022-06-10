01bbe244b909440e1e1bf127fb338d3ae49b7c28
gmarcha <gmarcha@github.com>
2022-06-10 16:00:30 -0400

    Fix pagename

--- client/pages/logout.vue
+++ client/pages/logout.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-  name: 'LoginPage',
+  name: 'LogoutPage',
 
   asyncData({ store, redirect }) {
     store.dispatch('logout')
