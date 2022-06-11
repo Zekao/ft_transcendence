1ab3b0590a29a8596298c3171d55a38a89a0d483
gmarcha <gmarcha@github.com>
2022-06-11 12:57:41 -0400

    Update profile page

--- client/pages/profile.vue
+++ client/pages/profile.vue
@@ -1,5 +1,41 @@
 <template>
-  <div></div>
+  <v-sheet height="100%">
+    <v-toolbar>
+      <v-tabs v-model="componentSelected">
+        <v-tab
+          v-for="component in componentList"
+          :key="component">
+          {{ component }}
+        </v-tab>
+      </v-tabs>
+      <v-text-field
+        v-model="usernameSelected"
+        prepend-icon="mdi-magnify"
+        label="Search user"
+        outlined
+        dense
+        hide-details
+        class="mr-2"/>
+      <v-btn outlined @click="usernameSelected = ''">
+        Clear
+      </v-btn>
+    </v-toolbar>
+    <v-tabs-items v-model="componentSelected">
+      <v-tab-item :key="componentList[0]">
+        <TestComponent/>
+      </v-tab-item>
+      <v-tab-item :key="componentList[1]">
+        <TestComponent/>
+      </v-tab-item>
+      <v-tab-item :key="componentList[2]">
+        <TestComponent/>
+      </v-tab-item>
+      <v-tab-item :key="componentList[3]">
+        <TestComponent/>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-sheet>
+
 </template>
 
 <script lang="ts">
@@ -8,5 +44,19 @@ export default Vue.extend({
   name: 'ProfilePage',
 
   middleware: 'auth',
+
+  data: () => ({
+    usernameSelected: '',
+    componentSelected: '',
+    componentList: [
+      'Infos', 'Friends', 'Match history', 'Stats',
+    ]
+  })
 })
 </script>
+
+<style>
+.v-window, .v-window__container, .v-window-item {
+  height: inherit
+}
+</style>
