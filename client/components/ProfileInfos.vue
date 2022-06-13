15f66b3cb065399e58566aff8ca9f8e294cab53a
gmarcha <gmarcha@github.com>
2022-06-12 20:39:22 -0400

    UFix null

--- client/components/ProfileInfos.vue
+++ client/components/ProfileInfos.vue
@@ -57,7 +57,7 @@ export default Vue.extend({
   name: 'ProfileInfos',
   middleware: 'auth',
   data: () => ({
-    file: null as Blob | null,
+    file: {} as Blob,
     isLoginValid: false,
     newLogin: '',
     loginRules: [
@@ -69,7 +69,7 @@ export default Vue.extend({
 
   computed: {
     ...mapState({
-      login: (state: any): string => state.user.authUser.display_name,
+      login: (state: any): string => state.user.authUser.display_name || '',
       avatar: (state: any): string => state.user.authUser.avatar,
       isTwoFactorAuth: (state: any): boolean => state.user.authUser.TwoFA,
     }),
@@ -85,7 +85,7 @@ export default Vue.extend({
         formData.append('image', this.file)
         try {
           await this.$store.dispatch('user/updateAuthAvatar', formData)
-          this.file = null
+          this.file = {} as Blob
         } catch (err) {
           console.log(err)
         }
