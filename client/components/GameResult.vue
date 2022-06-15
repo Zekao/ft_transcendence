77ac17275aa49ef343c51c6bf8197561f2a3482a
gmarcha <gmarcha@github.com>
2022-06-15 14:33:29 -0400

    Update

--- client/components/GameResult.vue
+++ client/components/GameResult.vue
@@ -4,14 +4,38 @@
     color="grey lighten-1"
     class="d-flex justify-center align-center"
   >
+    <v-list>
+      
+    </v-list>
     <v-btn color="primary" @click="$emit('next')"> Continue </v-btn>
-    <v-btn text> Cancel </v-btn>
   </v-card>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { mapState } from 'vuex'
+import { IMatch } from '~/store/match'
+
 export default Vue.extend({
   name: 'GameResult',
+
+  data: () => ({
+    match: {} as IMatch,
+  }),
+
+  computed: {
+    ...mapState({
+      selectedMatchId: (state: any) => state.selectedMatchId
+    })
+  },
+
+  async fetch() {
+    try {
+      const res = await this.$axios.$get(`/match/${this.selectedMatchId}`)
+      this.match = res
+    } catch(err) {
+      console.log(err)
+    }
+  }
 })
 </script>
