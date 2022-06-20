383d1b9dc86b51a8e5290e702fda823013f24e24
gmarcha <gmarcha@github.com>
2022-06-19 22:36:06 -0400

    Fix refresh spectate list

--- client/components/GameMatchmaking.vue
+++ client/components/GameMatchmaking.vue
@@ -59,7 +59,6 @@ export default Vue.extend({
   async fetch() {
     try {
       const res = await this.$axios.$get('/matchs?status=STARTED')
-      console.log(res)
       this.matches = res
     } catch (err: any) {
       if (err.response.status === 401) {
